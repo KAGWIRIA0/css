3f6895bbf1c83efd17e29ff6270416b3fbaa7bce
Faith <kagwiriafaith@gmail.com>
2024-10-28 11:30:24 -0700

    Added New python file.

--- lesson1.py
+++ lesson1.py
@@ -7,14 +7,3 @@ print("Tom","Juma","Mike")
 
 
 
-git init
-git add .
-git commit -m "lesson122nd"
-
-git config --global user.email "kagwiriafaith@gmail.com"
-git config --global user.name "Faith"
-
-git remote add origin https://github.com/KAGWIRIA0/bootstrap-basics..git
-
-git push -u origin master
-git push -u origin main
